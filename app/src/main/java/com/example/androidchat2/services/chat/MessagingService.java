fffe56d6be205630c583dfaae93fac844fbbd4b1
Matthias Brown <matthias.brown@epita.fr>
2021-09-17 21:40:38 +0200

    fix: adapt remote message to backend server
    use data field instead of notification field in order for the system to
    handle the notification making implicit deeplink to work better

--- app/src/main/java/com/example/androidchat2/services/chat/MessagingService.java
+++ app/src/main/java/com/example/androidchat2/services/chat/MessagingService.java
@@ -1,16 +1,16 @@
 package com.example.androidchat2.services.chat;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationChannelCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavDeepLinkBuilder;
 
 import com.example.androidchat2.R;
@@ -20,9 +20,11 @@ import com.google.firebase.messaging.RemoteMessage;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 public class MessagingService extends FirebaseMessagingService {
 
-    protected NotificationChannel newMessageChannel = null;
+    protected NotificationChannelCompat newMessageChannel = null;
 
     @Override
     public void onNewToken(@NonNull @NotNull String token) {
@@ -33,24 +35,23 @@ public class MessagingService extends FirebaseMessagingService {
     public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
         Logs.debug(this, "from: " + remoteMessage.getFrom());
 
-        RemoteMessage.Notification notification = remoteMessage.getNotification();
-        String groupId = remoteMessage.getData().get("groupId");
-
-        if (notification != null && groupId != null) {
-            sendNotification(notification, groupId);
+        Map<String, String> data = remoteMessage.getData();
+        if (data.containsKey("groupId")) {
+            sendNotification(data);
         }
     }
 
-    private void sendNotification(RemoteMessage.Notification notificationMessage, String groupId) {
+    private void sendNotification(Map<String, String> data) {
+        String groupId = data.get("groupId");
         PendingIntent pendingIntent = getGroupPendingIntent(groupId);
 
-        NotificationChannel newMessageChannel = getNewMessageNotificationChannel();
+        NotificationChannelCompat newMessageChannel = getNewMessageNotificationChannel();
 
-        Notification notification = getNewMessageNotification(newMessageChannel, notificationMessage, pendingIntent);
+        Notification notification = getNewMessageNotification(newMessageChannel, data, pendingIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.createNotificationChannel(newMessageChannel);
+
         notificationManager.notify(0, notification);
     }
 
@@ -65,31 +66,45 @@ public class MessagingService extends FirebaseMessagingService {
                 .createPendingIntent();
     }
 
-    public NotificationChannel getNewMessageNotificationChannel() {
+    public NotificationChannelCompat getNewMessageNotificationChannel() {
         if (newMessageChannel == null) {
             String channelId = getString(R.string.new_message_notification_channel_id);
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+                    .build();
 
-            newMessageChannel = new NotificationChannel(channelId,
-                    "Chat notification channel",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            newMessageChannel.setImportance(NotificationManager.IMPORTANCE_HIGH);
+            newMessageChannel = new NotificationChannelCompat.Builder(channelId, NotificationManagerCompat.IMPORTANCE_HIGH)
+                    .setName("Chat message channel")
+                    .setVibrationEnabled(true)
+                    .setVibrationPattern(new long[]{0, 500, 1000, 500})
+                    .setShowBadge(true)
+                    .setSound(defaultSoundUri, audioAttributes)
+                    .build();
         }
 
         return newMessageChannel;
     }
 
-    public Notification getNewMessageNotification(NotificationChannel notificationChannel, RemoteMessage.Notification notificationMessage, PendingIntent pendingIntent) {
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+    public Notification getNewMessageNotification(NotificationChannelCompat notificationChannel,
+                                                  Map<String, String> data,
+                                                  PendingIntent pendingIntent) {
+        String title = data.get("title");
+        String body = data.get("body");
 
         return new NotificationCompat
                 .Builder(this, notificationChannel.getId())
                 .setSmallIcon(R.drawable.ic_send_primary)
-                .setContentTitle(notificationMessage.getTitle())
-                .setContentText(notificationMessage.getBody())
-                .setSound(defaultSoundUri)
+                .setContentTitle(title)
+                .setContentText(body)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .setColor(getResources().getColor(R.color.blue_600, null))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+                .setNumber(1)
                 .build();
     }
 
